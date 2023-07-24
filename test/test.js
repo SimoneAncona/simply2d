081edd5796ab41affd4c352f86a23718f03dfb46
SimoneAncona <simone.ancona00@gmail.com>
2023-07-24 15:58:45 +0200

    test

--- test/test.js
+++ test/test.js
@@ -1,43 +1,10 @@
-import { Canvas, Colors } from "../index.js";
+import { Canvas, Colors } from "../index.js"
 
-const height = 500;
-const width = 500;
-const playerSize = 10;
-
-const canvas = new Canvas("title", height, width);
-
-let playerCurrentPos = { x: 20, y: height - playerSize };
-let playerVerticalSpeed = 0;
-let playerLateralSpeed = 0;
-
-const friction = 1;
-const gravity = 1;
-
-canvas.onKeysDown((keys) => {
-    if (keys.includes("Up")) {
-        if (playerCurrentPos.y === height - playerSize) {
-            playerVerticalSpeed = 10;
-            playerCurrentPos.y -= 1;
-        }
-    }
-    if (keys.includes("Left")) {
-        playerLateralSpeed = -10;
-    }
-    if (keys.includes("Right")) {
-        playerLateralSpeed = 10;
-    }
-});
-
-canvas.loop(() => {
-    canvas.drawRectangle(Colors.RED, playerCurrentPos, playerSize, playerSize, true);
-    if (playerCurrentPos.y <= height - playerSize) {
-        playerVerticalSpeed -= gravity;
-        playerCurrentPos.y -= playerVerticalSpeed;
-    } else {
-        playerCurrentPos.y = height - playerSize;
-    }
-    if (playerLateralSpeed !== 0) {
-        playerLateralSpeed < 0 ? playerLateralSpeed += friction : playerLateralSpeed -= friction;
-        playerCurrentPos.x += playerLateralSpeed;
-    }
-});
+const canvas = new Canvas("lines", 500, 500);
+const TOPL = { x: 0, y: 0 };
+const TOPR = { x: canvas.getWidth(), y: 0 };
+const BOTTOML = { x: 0, y: canvas.getHeight() };
+const BOTTOMR = { x: canvas.getWidth(), y: canvas.getHeight() }
+canvas.drawLine(Colors.BLUE, TOPL, BOTTOMR);
+canvas.drawLine(Colors.RED, TOPR, BOTTOML);
+canvas.sleep(1000);

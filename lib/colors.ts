c9416f6a921b6bbf5416686c44da3b48293cbb6e
SimoneAncona <simone.ancona00@gmail.com>
2023-08-22 17:05:17 +0200

    new colors

--- lib/colors.ts
+++ lib/colors.ts
@@ -9,14 +9,24 @@ export const Colors = {
 	MAGENTA: { red: 255, green: 0, blue: 255, alpha: 255 } as RGBAColor,
 	CYAN: { red: 0, green: 255, blue: 255, alpha: 255 } as RGBAColor,
 	DARK_BLUE: { red: 0, green: 0, blue: 139, alpha: 255 } as RGBAColor,
+	NAVY: { red: 0, green: 0, blue: 128, alpha: 255 } as RGBAColor,
 	LIGHT_BLUE: { red: 173, green: 216, blue: 230, alpha: 255 } as RGBAColor,
+	TEAL: { red: 0, green: 128, blue: 128, alpha: 255 } as RGBAColor,
+	DARK_CYAN: { red: 0, green: 139, blue: 139, alpha: 255 } as RGBAColor,
 	PURPLE: { red: 128, green: 0, blue: 128, alpha: 255 } as RGBAColor,
 	PINK: { red: 255, green: 192, blue: 203, alpha: 255 } as RGBAColor,
 	GRAY: { red: 192, green: 192, blue: 192, alpha: 255 } as RGBAColor,
 	GREY: { red: 192, green: 192, blue: 192, alpha: 255 } as RGBAColor,
 	ORANGE: { red: 255, green: 165, blue: 0, alpha: 255 } as RGBAColor,
 	AQUAMARINE: { red: 127, green: 255, blue: 212, alpha: 255 } as RGBAColor,
+	OLIVE: { red: 128, green: 128, blue: 0, alpha: 255 } as RGBAColor,
+	DARK_RED: { red: 139, green: 0, blue: 0, alpha: 255 } as RGBAColor,
+	SALMON: { red: 250, green: 128, blue: 114, alpha: 255 } as RGBAColor,
+	TOMATO: { red: 255, green: 99, blue: 71, alpha: 255 } as RGBAColor,
+	GOLD: { red: 255, green: 215, blue: 0, alpha: 255 } as RGBAColor,
+	DARK_GREEN: { red: 0, green: 100, blue: 0, alpha: 255 } as RGBAColor,
 	TRANSPARENT: { red: 0, green: 0, blue: 0, alpha: 0 } as RGBAColor,
+	INDIGO: { red: 75, green: 0, blue: 130, alpha: 0 } as RGBAColor,
 
 	from8bit(color256: number) {
 		if (color256 < 0 || color256 >= 256) throw "Color256 must be between 0 and 255"
@@ -39,7 +49,7 @@ export const Colors = {
 	},
 
 	from24bit(color: number) {
-		if (color < 0 || color >= 2**24) throw "Color must be between 0 and 2^24";
+		if (color < 0 || color >= 2 ** 24) throw "Color must be between 0 and 2^24";
 		return {
 			red: (color >> 16),
 			green: ((color & 0x00FF00) >> 8),
@@ -49,7 +59,7 @@ export const Colors = {
 	},
 
 	from32bit(color: number) {
-		if (color < 0 || color >= 2**32) throw "Color must be between 0 and 2^32";
+		if (color < 0 || color >= 2 ** 32) throw "Color must be between 0 and 2^32";
 		return {
 			red: (color >> 24),
 			green: ((color & 0x00FF0000) >> 16),

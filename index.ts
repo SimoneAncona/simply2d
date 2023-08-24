9ebadcf24546bd8cc90681d9527995b086c01156
SimoneAncona <simone.ancona00@gmail.com>
2023-08-24 15:39:24 +0200

    export key type

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { Canvas } from "./lib/canvas.js";
 export { Colors } from "./lib/colors.js"
-export { Position, RGBAColor, CanvasOptions, Resolution } from "./lib/types.js"
+export { Position, RGBAColor, CanvasOptions, Resolution, Key } from "./lib/types.js"
 export * from "./lib/sdl2int.js"

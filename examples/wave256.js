e3af2e9f0cf1c4eabe4b7a8ac61c852aad9707f7
SimoneAncona <simone.ancona00@gmail.com>
2024-09-14 01:50:12 +0200

    updated example

--- examples/wave256.js
+++ examples/wave256.js
@@ -1,8 +1,10 @@
 import { Canvas, Colors } from "../index.js"
 const canvas = new Canvas("wave256", 255, 255);
 
-for (let i = 0; i < canvas.getHeight(); i++) {
-	for (let j = 0; j < canvas.getWidth(); j++) {
-		canvas.drawPoint(Colors.from8bit(j), { x: j, y: i });
+canvas.loop(() => {
+	for (let i = 0; i < canvas.height; i++) {
+		for (let j = 0; j < canvas.width; j++) {
+			canvas.drawPoint(Colors.from8bit(j), { x: j, y: i });
+		}
 	}
-}
+})
